304c08b99e5a9f712ec236890cd2440505f24d6a
Huzaifa <162676983+dev-huzi-11@users.noreply.github.com>
2025-01-27 01:25:00 +0500

    update

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -4,13 +4,7 @@ import "./globals.css";
 import Provider from "../../lib/ThemeProvider";
 import Navbar from "../../components/Navbar";
 import Footer from "@/components/Footer";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
